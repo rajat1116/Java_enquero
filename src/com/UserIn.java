07183c778f6adba9467b6ca89cf522e067ece367
Rajat Pandit <rahulpandit106@gmail.com>
2019-06-17 19:10:04 +0530

    fourth commit

--- src/com/UserIn.java
+++ src/com/UserIn.java
@@ -1,4 +1,28 @@
 package com;
 
+import java.util.Scanner;
+
 public class UserIn {
+    public static void main(String[] args){
+        //creating a scanner object
+        Scanner input1 = new Scanner(System.in);
+        //output the promte
+        System.out.println("Enter a integer: ");
+        //get the user input
+        int value=input1.nextInt();
+        //throw the output
+        System.out.println("The value you input is: " + value);
+
+        Scanner input2=new Scanner(System.in);
+        System.out.println("Enter a string: ");
+        String Text=input2.nextLine();
+        System.out.println("The String you input: " + Text);
+
+        Scanner input3=new Scanner(System.in);
+        System.out.println("Enter a Floating: ");
+        double F=input3.nextDouble();
+        System.out.println("The double you input "+ F);
+
+    }
+
 }
