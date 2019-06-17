9fbb601725148535dfc59a4851d560f923d9649f
Rajat Pandit <rahulpandit106@gmail.com>
2019-06-17 19:14:18 +0530

    commit

--- src/com/Arrays.java
+++ src/com/Arrays.java
@@ -1,4 +1,20 @@
 package com;
 
 public class Arrays {
+    public static void main(String[] args){
+        int value=7;
+        int[] arr=new int[3];
+        arr[0]=10;
+        arr[1]=20;
+        arr[2]=30;
+        for(int i=0;i<arr.length;i++){
+            System.out.println(arr[i]);
+        }
+        int[] arr1={2,3,4};
+        for(int i=0;i<arr1.length;i++){
+            System.out.println(arr1[i]);
+        }
+
+    }
+
 }
