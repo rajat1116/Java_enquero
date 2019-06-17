5aa0c8f9164f4206debf209eebae287a8573dc87
Rajat Pandit <rahulpandit106@gmail.com>
2019-06-17 17:00:36 +0530

    First Commit

--- src/com/Variables.java
+++ src/com/Variables.java
@@ -1,29 +1,32 @@
 package com;
 
 public class Variables {
-    public static void main(String[] args) {
+        public static void main(String[] args){
 
-        boolean myBool = true;
-        System.out.println(myBool);
+            boolean myBool=true;
+            System.out.println(myBool);
 
-        byte myByte = 127;
-        System.out.println(myByte);
+            byte myByte=127;
+            System.out.println(myByte);
 
-        char myChar = 'R';
-        System.out.println(myChar);
+            char myChar='R';
+            System.out.println(myChar);
 
-        short myShort = 847;
-        System.out.println(myShort);
-        int myNumber = 88;
-        System.out.println(myNumber);
-        long myLong = 9898;
-        System.out.println(myLong);
+            short myShort=847;
+            System.out.println(myShort);
+            int myNumber=88;
+            System.out.println(myNumber);
+            long myLong=9898;
+            System.out.println(myLong);
 
-        float myFloat = 89.7f;
-        System.out.println(myFloat);
-        double myDouble = 8989.454;
-        System.out.println(myDouble);
+            float myFloat=89.7f;
+            System.out.println(myFloat);
+            double myDouble=8989.454;
+            System.out.println(myDouble);
 
 
+
+
+
+        }
     }
-}
