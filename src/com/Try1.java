b32abe17efeb8b9c891ea991c466743b381577c9
Rajat Pandit <rahulpandit106@gmail.com>
2019-06-19 10:53:43 +0530

    done

---
+++ src/com/Try1.java
@@ -0,0 +1,24 @@
+package com;
+class A{
+    int A(){
+
+    }
+    public void int A(){
+
+    }
+
+    public void show() {
+        System.out.println("Class A here");
+    }
+}
+class B extends A{
+    public void show() throws RuntimeException{
+        System.out.println("Class B here");
+    }
+}
+public class Try1 {
+    public static void main(String[] args){
+
+    }
+
+}
